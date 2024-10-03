e1719f35cec1da8389442c84465dcfb4eeb8f161
ducdoan1806 <ducdoan1806@gmail.com>
2024-10-03 15:30:21 +0700

    update

--- nail_be/settings.py
+++ nail_be/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 CORS_ALLOWED_ORIGINS = [
-    "*"
+    "http://localhost:3000"
     # Frontend URL if applicable
 ]
 LOGGING = {
