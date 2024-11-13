ef576c26fa1cded0d60c89edd176cbe6169c1db4
ducdoan1806 <ducdoan1806@gmail.com>
2024-11-13 14:30:40 +0700

    update

--- nail_be/settings.py
+++ nail_be/settings.py
@@ -60,15 +60,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:5173",
-    "http://localhost:8081",
-    "http://10.103.126.88:8081",
-    "http://nextjs:3000",
-    "http://admin:3001",
-    "http://localhost:80",
-    "http://localhost",
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3000",
     # Frontend URL if applicable
